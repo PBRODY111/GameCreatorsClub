264522214b9e5b88d104fee02e3fb4d85f090e6e
AF267 <47565887+airfrance267286@users.noreply.github.com>
2023-11-15 21:18:28 -0500

    test dial lock

--- Assets/Scripts/DialLock.cs
+++ Assets/Scripts/DialLock.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Radio : MonoBehaviour
+public class DialLock : MonoBehaviour
 {
-    public GameObject radio;
+    public GameObject dialLock;
+    public GameObject dialUI;
     public float interactionDistance;
-    public bool radioOn = true;
     public GameObject intText;
-    [SerializeField] private AudioSource radioAudio;
-    [SerializeField] private AudioSource clickAudio;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +20,11 @@ public class Radio : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, interactionDistance)){
-            //if(hit.collider.gameObject.name == radio.name){
-            if(hit.collider.gameObject.tag == "radio"){
+            if(hit.collider.gameObject.name == dialLock.name){
                 Debug.Log("Radio Hovered!");
                 intText.SetActive(true);
                 if(Input.GetKeyDown(KeyCode.E)){
-                    if(radioOn){
-                        radioAudio.Pause();
-                        clickAudio.Play(0);
-                        radioOn = !radioOn;
-                    } else {
-                        clickAudio.Play(0);
-                        radioAudio.Play(0);
-                        radioOn = !radioOn;
-                    }
-                    Debug.Log("Radio Clicked");
+                    dialUI.SetActive(true);
                 }
             } else {
                 intText.SetActive(false);
