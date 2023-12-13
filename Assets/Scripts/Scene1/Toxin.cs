04762a6e52381db794ab69203af551adbed0c2fa
AF267 <47565887+airfrance267286@users.noreply.github.com>
2023-12-13 14:31:26 -0500

    toxic levels

--- Assets/Scripts/Scene1/Toxin.cs
+++ Assets/Scripts/Scene1/Toxin.cs
@@ -1,25 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Toxin : MonoBehaviour
 {
+    public float toxicLevel = 0;
+    public float timer = 0;
+    public float timer1 = 0;
+    [SerializeField] private GameObject toxinUI;
+    [SerializeField] private Slider toxinSlider;
+    [SerializeField] private GameObject toxinOverlay;
+    private bool inTrigger = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    void OnCollisionEnter(Collision collision){
+    void OnTriggerStay(Collider collision){
         if (collision.gameObject.name == "Zagreus")
         {
-            Debug.Log("Player Collided!");
+            inTrigger = true;
+            if(toxicLevel <= 10){
+                timer += Time.deltaTime;
+
+                if (timer >= 0.1f){
+                    timer = 0f;
+                    toxicLevel += 0.1f;
+                }
+            } else{
+                SceneManager.LoadScene(4);
+            }
+            Debug.Log(toxicLevel);
+        }
+    }
+
+    void OnTriggerExit(Collider collision){
+        if (collision.gameObject.name == "Zagreus")
+        {
+            inTrigger = false;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(toxicLevel > 0){
+            toxinSlider.value = toxicLevel;
+            toxinOverlay.GetComponent<Image>().color = new Color(toxinOverlay.GetComponent<Image>().color.r, toxinOverlay.GetComponent<Image>().color.g, toxinOverlay.GetComponent<Image>().color.b, (toxicLevel/10));
+            toxinUI.SetActive(true);
+            if(!inTrigger){
+                timer1 += Time.deltaTime;
+
+                if (timer1 >= 0.3f){
+                    timer1 = 0f;
+                    toxicLevel -= 0.1f;
+                }
+            }
+        } else {
+            toxinUI.SetActive(false);
+            Debug.Log(toxicLevel + " Hidden");
+        }
     }
 }
