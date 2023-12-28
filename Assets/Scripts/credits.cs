f4b0b6a7d5d4c8eb02d78c61f041d2d6f3e0400b
AF267 <47565887+airfrance267286@users.noreply.github.com>
2023-12-28 16:59:22 -0500

    changed a bit for credits and menu

--- Assets/Scripts/credits.cs
+++ Assets/Scripts/credits.cs
@@ -9,6 +9,8 @@ public class credits : MonoBehaviour
     [SerializeField] private GameObject img1;
     [SerializeField] private GameObject img2;
     [SerializeField] private GameObject img3;
+    [SerializeField] private GameObject skipText;
+    private bool canSkip = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +18,22 @@ public class credits : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.E) && canSkip == true)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
     IEnumerator creditSequence()
     {
         // if first two endings wait 22 seconds, third ending on start, may have to change animations
         yield return new WaitForSeconds((float) 22.0);
         creditsAudio.Play();
-        yield return new WaitForSeconds((float) 168);
+        canSkip = true;
+        skipText.SetActive(true);
+        yield return new WaitForSeconds((float) 8);
+        skipText.SetActive(false);
+        yield return new WaitForSeconds((float) 160);
         img1.SetActive(true);
         yield return new WaitForSeconds((float) 1.0);
         img1.SetActive(false);
