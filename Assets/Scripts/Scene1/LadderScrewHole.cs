349723901d31087e169551d162721d47c531ca25
AF267 <47565887+airfrance267286@users.noreply.github.com>
2024-01-03 08:11:47 -0500

    ladder screw hole and music box music

---
+++ Assets/Scripts/Scene1/LadderScrewHole.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using TMPro;
+
+public class LadderScrewHole : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private GameObject intText3;
+    [SerializeField] private GameObject button2;
+    public UnityEvent rightClick;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+            rightClick.Invoke ();
+     }
+
+    // Start is called before the first frame update
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        intText3.GetComponent<TMP_Text>().text = "SCREWS NEEDED TO INTERACT";
+        intText3.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        intText3.SetActive(false);
+    }
+    public void ShowScrew(GameObject button){
+        button2.SetActive(true);
+        button.SetActive(false);
+    }
+}
