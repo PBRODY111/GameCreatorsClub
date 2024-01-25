b19fa882a39b1e3f232585aa6ec4ea38ed6c81a8
AF267 <47565887+airfrance267286@users.noreply.github.com>
2024-01-25 15:47:50 -0500

    fuckkkk it finally works!!

--- Assets/Scripts/Scene1/Poles.cs
+++ Assets/Scripts/Scene1/Poles.cs
@@ -21,6 +21,12 @@ namespace Scene1
         private Transform screw2Transform;
         private Transform screw3Transform;
         private Transform screw4Transform;
+        private float zRotation0;
+        private float zRotation1;
+        private float zRotation2;
+        private float zRotation3;
+        private float zRotation4;
+        private float epsilon = 0.001f;
 
         void Start()
         {
@@ -34,6 +40,11 @@ namespace Scene1
 
         void Update()
         {
+            zRotation0 = screw0Transform.rotation.eulerAngles.z;
+            zRotation1 = screw1Transform.rotation.eulerAngles.z;
+            zRotation2 = screw2Transform.rotation.eulerAngles.z;
+            zRotation3 = screw3Transform.rotation.eulerAngles.z;
+            zRotation4 = screw4Transform.rotation.eulerAngles.z;
             if (ladderUI.activeSelf && Input.GetKeyDown(KeyCode.Escape))
             {
                 ladderUI.SetActive(false);
@@ -51,12 +62,6 @@ namespace Scene1
                 intText.SetActive(false);
                 intText3.SetActive(false);
                 gameObject.SetActive(false);
-            } else{
-                Debug.Log("screw0"+CheckScrew0Conditions(screw0Transform));
-                Debug.Log("screw1"+CheckScrew1Conditions(screw1Transform));
-                Debug.Log("screw2"+CheckScrew2Conditions(screw2Transform));
-                Debug.Log("screw3"+CheckScrew3Conditions(screw3Transform));
-                Debug.Log("screw4"+CheckScrew4Conditions(screw4Transform));
             }
         }
 
@@ -91,6 +96,8 @@ namespace Scene1
                     Player.Player.Instance.UnlockCursor();
                 }
             }
+            Debug.Log((Approximately(zRotation3, 90f, epsilon) || Approximately(zRotation3, -90f, epsilon)));
+            Debug.Log(screw3Transform.GetComponent<RectTransform>().rect.width == 50);
         }
 
         private bool IsWithinReach()
@@ -98,44 +105,44 @@ namespace Scene1
             return Vector3.Distance(transform.position, Player.Player.Instance.transform.position) <= reach;
         }
 
+        bool Approximately(float a, float b, float epsilon)
+        {
+            return Mathf.Abs(a - b) < epsilon;
+        }
+
         bool CheckScrew0Conditions(Transform screw0Transform)
         {
             // Check if Z rotation is 45 or -135 and width is 50
-            return ((screw0Transform.localRotation.eulerAngles.z == 45 ||
-                    screw0Transform.localRotation.eulerAngles.z == -135) &&
+            return ((Approximately(zRotation0, 45f, epsilon) || Approximately(zRotation0, -135f, epsilon)) &&
                 screw0Transform.GetComponent<RectTransform>().rect.width == 50);
         }
 
         bool CheckScrew3Conditions(Transform screw3Transform)
         {
             // Check if Z rotation is 90 or -90 and width is 50
-            return ((screw3Transform.localRotation.eulerAngles.z == 90 ||
-                    screw3Transform.localRotation.eulerAngles.z == -90) &&
+            return ((Approximately(zRotation3, 90f, epsilon) || Approximately(zRotation3, -90f, epsilon)) &&
                 screw3Transform.GetComponent<RectTransform>().rect.width == 50);
         }
 
         bool CheckScrew1Conditions(Transform screw1Transform)
         {
             // Check if Z rotation is 90 or -90 and width is 50
-            return ((screw1Transform.localRotation.eulerAngles.z == -45 ||
-                    screw1Transform.localRotation.eulerAngles.z == 135) &&
+            return ((Approximately(zRotation1, -45f, epsilon) || Approximately(zRotation1, 135f, epsilon)) &&
                 screw1Transform.GetComponent<RectTransform>().rect.width == 50);
         }
 
         bool CheckScrew4Conditions(Transform screw4Transform)
         {
             // Check if Z rotation is 90 or -90 and width is 50
-            return ((screw4Transform.localRotation.eulerAngles.z == 0 ||
-                    screw4Transform.localRotation.eulerAngles.z == 180) &&
-                screw4Transform.GetComponent<RectTransform>().rect.width == 50);
+            return ((Approximately(zRotation4, 0f, epsilon) || Approximately(zRotation4, 180f, epsilon)) &&
+                screw4Transform.GetComponent<RectTransform>().rect.width == 80);
         }
 
         bool CheckScrew2Conditions(Transform screw2Transform)
         {
             // Check if Z rotation is 90 or -90 and width is 50
-            return ((screw2Transform.localRotation.eulerAngles.z == 90 ||
-                    screw2Transform.localRotation.eulerAngles.z == -90) &&
-                screw2Transform.GetComponent<RectTransform>().rect.width == 50);
+            return ((Approximately(zRotation2, 90f, epsilon) || Approximately(zRotation2, -90f, epsilon)) &&
+                screw2Transform.GetComponent<RectTransform>().rect.width == 80);
         }
     }
 }
